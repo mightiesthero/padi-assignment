c68f3e8744f99127b213faced07832bcbe660fcd
Bengsiswanto <mightiesthero@gmail.com>
2019-04-14 10:56:08 +0700

    add begin rescue send email

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -14,19 +14,25 @@ class ApiController < ApplicationController
   end
 
   def submit
+    email_status = "send email success"
     survey = @user.survey_sites.new(submit_params)
 
     message = "Survey saved"
     if survey.save
       link = "#{request.protocol}#{request.host_with_port}#{survey_site_path(survey)}"
       message = "#{message} #{link}"
-      UserMailer.survey(params[:email], survey, link).deliver_now
+      begin
+        UserMailer.survey(params[:email], survey, link).deliver_now
+      rescue
+        email_status = "send email error"
+      end
     else
       message = survey.errors.full_messages
     end
 
     result = {
       message: message
+      email_status: email_status
     }
     render json: result.to_json
   end
@@ -37,6 +43,7 @@ class ApiController < ApplicationController
     end
 
     def submit_params
+      # TODO: convert sales email to id
       # sales = User.find_by(email: params[:sales])
       # params[:user_id] = sales.id
       params.permit(:clientname, :address, :city, :longitude, :latitude, :status, :sales_id, :user_id)
